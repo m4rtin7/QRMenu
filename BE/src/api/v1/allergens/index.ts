a42f0591f0b8b7dd2e4262ae38ab3c5937154500
Danina Valeriya <valeriya.danina@extern.a1.at>
2022-12-15 09:04:26 +0100

    r

--- BE/src/api/v1/allergens/index.ts
+++ BE/src/api/v1/allergens/index.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express'
+import passport from 'passport'
+import permissionMiddleware from '../../../middlewares/permissionMiddleware'
 
 import validationMiddleware from '../../../middlewares/validationMiddleware'
+import { PERMISSION } from '../../../utils/enums'
 
 import * as getMenuItems from './get.allergens'
+//import * as deletemMenuItem from './delete.menuItem'
 
 const router = Router({ mergeParams: true })
 
